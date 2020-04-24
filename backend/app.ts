a5cb5b374cd1d2837a11c0427d7c58a7aab6ccb8
Bill <bill@goinggone.io>
2020-04-24 14:52:41 +0100

    resource badge link bug

--- backend/app.ts
+++ backend/app.ts
@@ -2,22 +2,14 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyparser from 'body-parser';
 import requestLoggerMiddleware from './request.logger.middleware';
-// import cloudinaryRouter from './routes/cloudinary';
 import ftpRouter from './routes/ftp';
 
 const path = require('path');
 const shrinkRay = require('shrink-ray-current');
-const cloudinary = require('cloudinary').v2;
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
 
 const app = express();
 
@@ -28,7 +20,6 @@ app.use(cors());
 app.use(bodyparser.json());
 app.use(requestLoggerMiddleware);
 
-// app.use('/cloudinary', cloudinaryRouter);
 app.use('/ftp', ftpRouter);
 
 
