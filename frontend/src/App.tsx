3ea92244d95e1f4d8f2c55a051d7ee2d0b423a0f
Bill <bill@goinggone.io>
2020-05-08 14:26:03 +0100

    added google analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Router, Route, Switch } from "react-router-dom"
 import Home from './components/home/Home'
 import HttpsRedirect from 'react-https-redirect'
-// import ReactGA from'react-ga'
+import ReactGA from'react-ga'
 import { createBrowserHistory } from 'history';
 import DynamicComponent from './components/dynamicComponent/DynamicComponent'
 import MenuBar from './components/shared/MenuBar'
@@ -10,6 +10,14 @@ import Footer from './components/shared/Footer';
 import About from './components/about/About';
 
 const history = createBrowserHistory();
+
+ReactGA.initialize('UA-165948277-1');
+
+
+history.listen((location) => {
+    ReactGA.set({ page: location.pathname + location.search })
+    ReactGA.pageview(location.pathname + location.search)
+});
 
 const App = () => {
 
